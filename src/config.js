f3c6720b16acc62d62d093ed37290ec149e796bd
Sabrina Becker <sbecka8@gmail.com>
2020-12-06 00:07:27 -0500

    Update now to vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL,
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
   APP_VERSION: '1.1.1'
 };
